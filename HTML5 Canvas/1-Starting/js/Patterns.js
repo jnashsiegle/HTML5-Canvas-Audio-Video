4761e939be43d801512c74b55215ca9716323f2a
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-07 17:42:08 -0500

    patterns

--- HTML5 Canvas/1-Starting/js/Patterns.js
+++ HTML5 Canvas/1-Starting/js/Patterns.js
@@ -3,10 +3,44 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-		
-			
+
+		/*//Create a pattern from an image
+
+		var patImg = new Image();
+
+		//Once the image loads, then can actually use it
+
+		patImg.onload = function(){
+			ctx.fillStyle = ctx.createPattern(patImg, "repeat");
+			ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);//sets width n height so it can be changed via html later and not needed to change in two places
+
+		};
+
+		patImg.src = "images/flowers.jpg"; */
+
+		//Create a pattern from another canvas
+
+		var patCanvas = document.getElementById("patCan");
+		var patCtx = patCanvas.getContext("2d");
+
+		//Draw a red line in our pattern canvas
+
+		patCtx.strokeStyle = "red";
+		patCtx.lineWidth = 1;
+		patCtx.beginPath();
+		patCtx.moveTo(0,0);
+		patCtx.lineTo(25,25);
+		patCtx.stroke();
+
+		//Use pattern canvas as an outline on our larger canvas
+
+		var strokePat = ctx.createPattern(patCanvas, "repeat"); //larger canvas context
+		ctx.strokeStyle = strokePat;
+		ctx.lineWidth = 25;
+		ctx.strokeRect(50, 50, 200, 200);	
 				
 						
+			
 		}
 	}
 }
--- HTML5 Canvas/1-Starting/js/Shadows.js
+++ HTML5 Canvas/1-Starting/js/Shadows.js
@@ -3,7 +3,52 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
+
+		ctx.save();  //variable way #2 of removing shadow, using restore below
+		var origShadowColor = ctx.shadowColor;  //enables us to remove shadow later variable #1
+
+		//set up our shadow
+		ctx.shadowColor = "#000000";
+		ctx.shadowOffsetX = 10;
+		ctx.shadowOffsetY = 10;
+		ctx.shadowBlur = 10; 
 		
+		//draw a simple rectangle
+		ctx.fillStyle = "blue";
+		ctx.fillRect(20, 20, 200, 100);
+
+		//draw some text with shadow   Shadows effect everything below it
+		ctx.fillStyle = "green";
+		ctx.shadowColor = "rgba(0, 100, 100, .5)";
+		ctx.shadowOffsetX = 5;
+		ctx.shadowOffsetY = 5;
+		ctx.shadowBlur = 5;
+
+		ctx.font = "25pt Georgia";
+		ctx.fillText("Drawing Text on a Canvas!!", 250, 75);
+
+		//Draw a red line with a purple shadow
+
+		ctx.lineCap = "round";
+		ctx.lineWidth = 25;
+		ctx.shadowColor = "rgba(150, 0, 150, .5)";
+		ctx.shadowOffsetX = 5;
+		ctx.shadowOffsetY = 5;
+		ctx.shadowBlur = 15;
+
+		ctx.strokeStyle = "red";
+		ctx.beginPath();
+		ctx.moveTo(50, 200);
+		ctx.lineTo(450, 200);
+		ctx.stroke();
+
+		//Rectangle with no shadow
+		//ctx.shadowColor = origShadowColor;  //removes shadow color variable way 1
+
+		ctx.restore();  //remove shadow variable way #2 - resets to default values, preferred method
+
+		ctx.fillStyle = "blue";
+		ctx.fillRect(20, 225, 200, 50);
 				
 						
 		}
