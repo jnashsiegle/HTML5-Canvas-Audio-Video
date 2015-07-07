58075644ae3670c8a0916d5c27252bb8dad98ec1
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-07 16:11:27 -0500

    complex curves

--- HTML5 Canvas/1-Starting/js/Curves.js
+++ HTML5 Canvas/1-Starting/js/Curves.js
@@ -3,6 +3,29 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
+		ctx.strokeStyle = "blue";
+		ctx.lineWidth = 5;
+
+		//Stroke a simple bezier curve
+
+		ctx.beginPath();
+		ctx.moveTo(50, 200);
+
+		//bezierCurveTo(cx1, cy1, cx2, cy2, x, y,)*cx cy are control points of the bezierCurve
+
+		ctx.bezierCurveTo(50, 100, 200, 300, 200, 150);
+
+		ctx.stroke();
+
+		//quadratic curve
+
+		ctx.beginPath();
+		ctx.moveTo(400, 200);
+
+		//quadraticCurveTo(cx, cy, x, y);
+
+		ctx.quadraticCurveTo(400, 100, 600, 150);
+		ctx.stroke();
 				
 						
 						
