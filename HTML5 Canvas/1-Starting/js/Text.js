51f776357c01c0ca887acf444298f8dea4188474
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-07 16:39:52 -0500

    text

--- HTML5 Canvas/1-Starting/js/Text.js
+++ HTML5 Canvas/1-Starting/js/Text.js
@@ -4,6 +4,25 @@ window.onload = function() {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
 			
+			var theString = "Drawing text on a Canvas";
+
+			//Simple Text using defaults
+			ctx.fillText(theString, 20, 20);//20 to right and 20 down
+			
+			//Styling
+			ctx.font = "25pt Georgia";
+			ctx.fillStyle = "blue";
+			ctx.fillText(theString, 20, 60);
+
+			//stroke and filled text
+			ctx.font = "32pt Verdana";
+			ctx.fillStyle = "yellow";
+			ctx.textBaseline = "middle";//puts the baseline into the middle of text
+			ctx.strokeStyle = "rgba(0, 255, 0, .8)";//use all css/rgb or rgba colors
+			ctx.fillText(theString, 20, 160);
+			ctx.strokeText(theString, 20, 160);
+
+
 			
 						
 		}
