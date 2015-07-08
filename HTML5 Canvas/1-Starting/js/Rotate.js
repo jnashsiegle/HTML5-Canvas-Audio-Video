71d16f20c5907863ccf576013141b1135fbb6e39
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-07 20:23:42 -0500

    transforms

--- HTML5 Canvas/1-Starting/js/Rotate.js
+++ HTML5 Canvas/1-Starting/js/Rotate.js
@@ -3,7 +3,41 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-	
+		
+		/*
+		ctx.fillStyle = "blue";
+		ctx.fillRect(150, 50, 100, 50);
+
+		ctx.translate(200, 75); 
+		
+		ctx.rotate(.5);
+		ctx.fillStyle = "red";
+		ctx.fillRect(-50, -25, 100, 50);  //rotates around on top of first rectangle
+*/
+		var radians = (Math.PI/180)*20; //making a starburst 20 is degrees
+
+		ctx.translate(ctx.canvas.width/2, ctx.canvas.height/2);
+		//for(var degrees = 0; degrees<360; degrees+=20){
+		for (var degrees = 0; degrees<=180; degrees+=20){ //half circle
+		ctx.beginPath();
+		ctx.moveTo(0,0 );
+		ctx.lineTo(100, 0); //100 pixels long
+		ctx.stroke();
+		ctx.rotate(radians);
+
+		
+
+
+		}
+
+		/*
+		ctx.beginPath();
+		ctx.moveTo(0,0 );
+		ctx.lineTo(100, 0); //100 pixels long
+		ctx.stroke(); */
+
+
+
 			
 						
 		}
--- HTML5 Canvas/1-Starting/js/Scale.js
+++ HTML5 Canvas/1-Starting/js/Scale.js
@@ -3,7 +3,21 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-	
+		
+		ctx.save();
+		ctx.fillStyle = "blue";
+		ctx.fillRect(0,0, 100, 50);
+
+		//scale transform
+
+		ctx.scale(1.5, 2); //150% bigger //also scales whitespace between objects if more than one
+		ctx.fillRect(0, 50, 100, 50);
+
+		//Create a third rectangle  IMPORTANT scaling is additive so use save and restore
+		ctx.restore();
+		ctx.fillStyle = "purple";
+		ctx.scale(.5, .5);//
+		ctx.fillRect(0, 100, 100, 50);
 						
 		}
 	}
