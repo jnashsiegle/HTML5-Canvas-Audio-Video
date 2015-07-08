a9a9c6289013dde25c647a545c13f5c81a8c0ad6
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-07 19:53:33 -0500

    animation

--- HTML5 Canvas/1-Starting/js/Animation.js
+++ HTML5 Canvas/1-Starting/js/Animation.js
@@ -3,7 +3,24 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-				
+		
+		var srcImg = document.getElementById("img1");
+
+		var rotation = 0;
+
+		
+
+		ctx.translate(theCanvas.width/2, theCanvas.height/2);
+
+		setInterval(function(){
+
+		rotation +=8;
+
+		ctx.clearRect(-300, -300, theCanvas.width, theCanvas.height);
+
+		ctx.rotate(rotation * (Math.PI/180));
+		ctx.drawImage(srcImg, -75, -75);
+		}, 500);		
 			
 						
 						
