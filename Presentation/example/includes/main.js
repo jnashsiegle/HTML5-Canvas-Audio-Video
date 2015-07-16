2063ebdc8cdd7581d536050e5f0ea14a55700d50
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-16 17:33:16 -0500

    example complete

--- Presentation/example/includes/main.js
+++ Presentation/example/includes/main.js
@@ -1,22 +1,28 @@
-var fadein_tween = TweenMax.to('#fadein-trigger > div', .375,{
- opacity: 1
-  });
-var fadeout_tween = TweenMax.to('#fadein-trigger > div', .375,{ 
-  opacity: 0 
-});
-
-var controller = new ScrollMagic.Controller();
-
-var fadein_scene = new ScrollMagic.Scene({
-  triggerElement: '#fadein-trigger',
-  reverse: true
-})
-.setTween(fadein_tween)
-.addTo(controller);
-
-var fadeout_scene = new ScrollMagic.Scene({
-  triggerElement: '#fadeout-trigger',
-  reverse: true
-})
-.setTween(fadeout_tween)
-.addTo(controller);
+jQuery(function( $ ){
+      /**
+       * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
+       * @see http://demos.flesler.com/jquery/scrollTo/
+       * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.localScroll.
+       */
+      
+      // The default axis is 'y', but in this demo, I want to scroll both
+      // You can modify any default like this
+      $.localScroll.defaults.axis = 'xy';
+      
+      /**
+       * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
+       * also affect the >> and << links. I want every link in the page to scroll.
+       */
+      $.localScroll({
+        target: '#content', // could be a selector or a jQuery object too.
+        queue:true,
+        duration:1000,
+        hash:true,
+        onBefore:function( e, anchor, $target ){
+          // The 'this' is the settings object, can be modified
+        },
+        onAfter:function( anchor, settings ){
+          // The 'this' contains the scrolled element (#content)
+        }
+      });
+    });
