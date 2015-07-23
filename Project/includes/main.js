863d06a0f6de920d8dcb9801650b7518e1de418a
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-07-22 22:58:04 -0500

    ...

--- Project/includes/main.js
+++ Project/includes/main.js
@@ -4,21 +4,27 @@ window.onload = function() {
 	//text class is for working with text
 	var sentence = new createjs.Text();
 	var sentence2 = new createjs.Text();
+	
+	
+
 	//modify the text property
 	sentence.text = "LightPoint";
 	sentence2.text = "Development"
-	sentence.font = "bold 60px Verdana";
-	sentence2.font = "bold 60px Verdana";
+	sentence.font = "bold 80px 'Goudy Old Style'";
+	sentence2.font = "bold 80px 'Goudy Old Style'";
 	sentence.color = "#657036";
 	sentence2.color = "#657036";
+
 	//text IS a display object
 	//it inherits all of the normal properties
-	sentence.x = 100;
+	sentence.x = 50;
 	sentence.y = 100;
-	sentence2.x = 200;
-	sentence2.y = 175;
+	sentence2.x = 100;
+	sentence2.y = 195;
+	
 
 
+	
 	stage.addChild(sentence);
 	stage.addChild(sentence2);
 	stage.update();	
@@ -32,6 +38,18 @@ $(document).ready(function() {
 /*++++++++++++++++Let's make a hidden navigation set ++++++++++++++ */
 $(document).ready(function(){
     $( '.menu-btn' ).click(function(){
-    $('.responsive-menu').toggleClass('expand')
+    $('.hamburger').toggleClass('expand')
    })
  })
+
+$(document).ready(function(){
+	$( '#overlay').click(function(){
+	overlay.appendTo(document.body)
+	});
+});
+
+/*+++++++++++++++++Let's make a tagline++++++++++++++++*/
+$().ready(function() {
+	$(upArc).arctext({radius: 300})
+	$(downArc).arctext({radius: 400, dir: -1});
+});
